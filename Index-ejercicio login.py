a1a6801652adf7de31e73407005108d639c73d00
Gonth14 <ar.gonthier@duocuc.cl>
2026-04-15 09:45:28 -0400

    Add files via upload

---
+++ Index-ejercicio login.py
@@ -0,0 +1,18 @@
+while True:
+     
+ usuario = input("¿Cual es tu nombre de usuario? ")
+
+ if usuario == "Ariel" or usuario == "ariel":
+    print ("Hola, Ariel\n")
+    break
+ else:
+    print ("Usuario incorrecto, Reingrese usuario \n")
+
+while True:
+ Clave = input("Ingrese su contraseña ")
+
+ if Clave == "123":
+     print ("\nUsuario y Contraseña correcta. Bienvenido")
+     break
+ else:
+     print("contraseña incorrecta, Ingrese denuevo la contraseña \n")
